01628412c6e63b153da552897156702632600cec
Aleksey Plutaev <plutaef@gmail.com>
2022-05-22 15:09:01 +0600

    new page

---
+++ components/table.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="table">
+        <template v-for="tableColl, i of tableData">
+            <div class="table__coll" :key="i">
+                <div class="table__row" v-for="tableRow, i of tableColl" :key="i">
+                    <input type="text" class="table__input" :value="tableRow">
+                </div>
+            </div>
+        </template>
+    </div>    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            row: 7,
+            coll: 6
+        }
+    },
+    props: [
+        'tableData'
+    ]
+}
+</script>
+
+<style lang="scss">
+.table {
+    width: 100%;
+    display: flex;
+    &__input {
+        width: 225px;
+        height: 100px;
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 30px;
+        line-height: 37px;
+        text-align: center;
+        color: #434242;
+        background: transparent;
+        border: none;
+    }
+}
+</style>
